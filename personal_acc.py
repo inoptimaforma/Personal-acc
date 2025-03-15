792d1bc5c38b0096f197c2602433c4fcc88b894c
Zhamal <150662063+inoptimaforma@users.noreply.github.com>
2025-03-15 22:11:14 +0600

    Update personal_acc.py

--- personal_acc.py
+++ personal_acc.py
@@ -2,27 +2,27 @@ class PersonalAccount:
     def __init__(self, account_num: int, account_holder: str):
         self.account_num = account_num
         self.account_holder = account_holder
-        self.balance = 0.0
+        self.__balance = 0.0
         self.transactions = []
 
     def deposit(self, amount: float):
         transaction = Amount(amount, 'DEPOSIT')
         self.transactions.append(transaction)
-        self.balance += amount
+        self.__balance += amount
 
     def withdraw(self, amount: float):
-        if amount > self.balance:
+        if amount > self.__balance:
             print("Not enough money")
             return
         transaction = Amount(amount, 'WITHDRAWAL')
         self.transactions.append(transaction)
-        self.balance -= amount
+        self.__balance -= amount
 
     def transaction_history(self):
         for transaction in self.transactions:
             print(transaction)
     def get_balance(self):
-        return self.balance
+        return self.__balance
     def get_account_num(self):
         return self.account_num
     def set_account_num(self, account_num: int):
@@ -32,7 +32,7 @@ class PersonalAccount:
     def set_account_holder(self, account_holder: str):
         self.account_holder = account_holder
     def __str__(self):
-        return f"Account Number: {self.account_number}\nAccount Holder: {self.account_holder}\nBalance: ${self.balance:.2f}"
+        return f"Account Number: {self.account_number}\nAccount Holder: {self.account_holder}\nBalance: ${self.__balance:.2f}"
     def __add__(self, amount: float):
         self.deposit(amount)
         return self
